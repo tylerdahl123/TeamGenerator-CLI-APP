862fa4d9bce6874288d9f3a04280419a70234ce1
tylerdahl123 <dahlgren15@gmail.com>
2020-11-30 16:11:35 -0800

    started with the classes and sub classes...c/p class examples

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -5,6 +5,18 @@ class Employee {
         this.id=id;
         this.email=email
     }
+    getName () {
+        return this.name;
+    }
+    getId () {
+        return this.id;
+    }
+    getRole () {
+        return "employee";
+    }
+    getEmail(){
+        return this.email;
+    }
 }
 
 module.exports = Employee;
